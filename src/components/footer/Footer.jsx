ee460fba3ba819401b100b40fb78a139e03e9e5c
Oumide Ajidahun <wenceslausajidahun@gmail.com>
2024-04-05 20:30:09 +0200

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./footer.css";
+import { CiFacebook } from "react-icons/ci";
+import { FaXTwitter } from "react-icons/fa6";
+import { CiInstagram } from "react-icons/ci";
+
 
 const Footer = () => {
   return (
+
     <div className="footer">
       <div className="footerItems">
         <h2>About</h2>
 
         <div className="horiz">
-            <div className="hoziz_child"></div>
+          <div className="hoziz_child"></div>
         </div>
 
         <p>
@@ -19,11 +24,50 @@ const Footer = () => {
         </p>
       </div>
 
-      <div className="footerItems">2</div>
+      <div className="footerItems">
+        <h2>What do you think of CFD trading?</h2>
+        <p>October 18, 2023.</p>
+      </div>
 
-      <div className="footerItems">3</div>
+      <div className="footerItems">
+        <h2>Recent Articles</h2>
 
-      <div className="footerItems">4</div>
+        <div className="horiz">
+          <div className="hoziz_child"></div>
+        </div>
+        <h2>What do you think of CFD trading?</h2>
+        <p>October 18, 2023.</p>
+
+        <h2>What do you think of CFD trading?</h2>
+        <p>October 18, 2023.</p>
+
+        <h2>What do you think of CFD trading?</h2>
+        <p>October 18, 2023.</p>
+
+
+      </div>
+
+      <div className="footerItems">
+        <h2>In the News</h2>
+
+        <div className="horiz">
+          <div className="hoziz_child"></div>
+        </div>
+        <h2>What do you think of CFD trading? Give your opinion in the comments below!😉</h2>
+        <p>October 18, 2023.</p>
+
+        <h2>What do you think of CFD trading? Give your opinion in the comments below!😉</h2>
+        <p>October 18, 2023.</p>
+
+        <h2>What do you think of CFD trading? Give your opinion in the comments below!😉</h2>
+        <p>October 18, 2023.</p>
+
+        <div className="footerIcons">
+          <CiFacebook />
+          <FaXTwitter />
+          <CiInstagram />
+        </div>
+      </div>
     </div>
   );
 };
